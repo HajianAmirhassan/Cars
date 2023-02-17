4a2f783695c65e1bb798622c9f5daea531b07cfb
AMIRHASSAN Hajian <amirhassanhajian@gmail.com>
2023-02-17 22:32:24 +0330

    Data model car and car cat and Foreignkey

---
+++ Models/Entities/Car.cs
@@ -0,0 +1,34 @@
+﻿using Cars.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Cars.Models.Entity
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Model { get; set; }
+
+        public int Age { get; set; }
+
+        public int Type { get; set; }
+
+        public int CarCat_Id { get; set; }
+        [ForeignKey("CarCat_Id")]
+        public virtual CarCat CarCat { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public Car()
+        {
+            CreatedDate = DateTime.Now;
+        }
+    }
+
+}
---
+++ Models/Entities/CarCat.cs
@@ -0,0 +1,23 @@
+﻿using Cars.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Cars.Models.Entities
+{
+    public class CarCat
+    {
+        public int id { get; set; }
+
+        public string Title { get; set; }
+
+        public virtual ICollection<Car> CarCollection { get; set; }
+
+        public CarCat()
+        {
+            CarCollection = new List<Car>();
+        }
+    }
+}
