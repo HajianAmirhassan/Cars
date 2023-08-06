f548175e1a79ba7d3e64ff704792707a5f6df4ce
AMIRHASSAN Hajian <amirhassanhajian@gmail.com>
2023-08-06 06:30:50 +0330

    add setting

---
+++ Models/Entities/Setting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cars.Models.Entities
+{
+    public class Setting
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Layout { get; set; }
+
+        public string Keyword { get; set; }
+    }
+}
