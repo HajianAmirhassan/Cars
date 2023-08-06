7a3675077823d5030ca0d452851c11b64398c303
AMIRHASSAN Hajian <amirhassanhajian@gmail.com>
2023-08-06 06:30:34 +0330

    add migration

---
+++ Migrations/202307210925265_Car_Mig1.cs
@@ -0,0 +1,29 @@
+﻿namespace Cars.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Car_Mig1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Settings",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Description = c.String(),
+                        Layout = c.String(),
+                        Keyword = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Settings");
+        }
+    }
+}
